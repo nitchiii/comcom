146e2a9e6f746eb39f393a7f849fa778c35a851c
John Rae G. Manuel <garciajohnmanuel016@gmail.com>
2024-09-09 19:18:52 +0800

    Hello

---
+++ app/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>not a footer</div>
+  )
+}
+
+export default Footer
---
+++ app/Header/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>not a header</div>
+  )
+}
+
+export default Header
